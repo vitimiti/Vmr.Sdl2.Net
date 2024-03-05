db0744e926d700455e03ce837bbc5dab025c4a64
Victor Matia <vmatir@gmail.com>
2024-03-05 10:39:50 +0000

    Add Sdl class for OS-specific DLL import
    A new Sdl class was added in the Imports namespace of our SDL2 .NET
    library to handle the OS-specific DLL imports.
    
    It uses a KeyValuePair list to map different OS to their corresponding
    DLL file names.
    
    The class also provides a DllImportResolver to load the right DLL based
    on the running OS.
    
    Signed-off-by: Victor Matia <vmatir@gmail.com>

---
+++ Vmr.Sdl2.Net/Imports/Sdl.cs
@@ -0,0 +1,44 @@
+// Copyright (c) 2024 Victor Matia <vmatir@gmail.com>
+
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+namespace Vmr.Sdl2.Net.Imports;
+
+internal static partial class Sdl
+{
+    private static readonly List<KeyValuePair<bool, string>> OsSpecificNames =
+    [
+        new KeyValuePair<bool, string>(OperatingSystem.IsWindows(), "SDL2.dll"),
+        new KeyValuePair<bool, string>(OperatingSystem.IsMacOS(), "libSDL2.dylib"),
+        new KeyValuePair<bool, string>(
+            OperatingSystem.IsLinux() || OperatingSystem.IsFreeBSD(),
+            "libSDL2-2.0.so.0"
+        )
+    ];
+
+    private static string GetOsSpecificName(string libName)
+    {
+        return OsSpecificNames.FirstOrDefault(valuePair => valuePair.Key).Value ?? libName;
+    }
+
+    private static nint DllImporter(
+        string libName,
+        Assembly assembly,
+        DllImportSearchPath? searchPath
+    )
+    {
+        _ = NativeLibrary.TryLoad(
+            GetOsSpecificName(libName),
+            assembly,
+            searchPath,
+            out nint handle
+        );
+        return handle;
+    }
+
+    static Sdl()
+    {
+        NativeLibrary.SetDllImportResolver(typeof(Sdl).Assembly, DllImporter);
+    }
+}
