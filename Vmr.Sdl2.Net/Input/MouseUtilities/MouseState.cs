dd060f413d0b16d301e2ac1e3edda05b53ac6837
Victor Matia <vmatir@gmail.com>
2024-03-08 18:55:30 +0000

    Implement IEquatable in MouseState and KeySym structs
    The MouseState and KeySym structs now implement the IEquatable
    interface, and overridden methods for Equals(), GetHashCode(), and
    ToString() have been defined.
    
    These changes enable comparability and better string representation of
    these structs, benefiting performance in collections and improving
    debugging experience.
    
    Signed-off-by: Victor Matia <vmatir@gmail.com>

--- Vmr.Sdl2.Net/Input/MouseUtilities/MouseState.cs
+++ Vmr.Sdl2.Net/Input/MouseUtilities/MouseState.cs
@@ -4,8 +4,38 @@ using System.Drawing;
 
 namespace Vmr.Sdl2.Net.Input.MouseUtilities;
 
-public readonly struct MouseState
+public readonly struct MouseState : IEquatable<MouseState>
 {
     public MouseButtons Buttons { get; internal init; }
     public Point Position { get; internal init; }
+
+    public bool Equals(MouseState other)
+    {
+        return Buttons == other.Buttons && Position.Equals(other.Position);
+    }
+
+    public override bool Equals(object? obj)
+    {
+        return obj is MouseState other && Equals(other);
+    }
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine((int)Buttons, Position);
+    }
+
+    public override string ToString()
+    {
+        return $"{{Buttons: [{Buttons}], Position: {Position}}}";
+    }
+
+    public static bool operator ==(MouseState left, MouseState right)
+    {
+        return left.Equals(right);
+    }
+
+    public static bool operator !=(MouseState left, MouseState right)
+    {
+        return !left.Equals(right);
+    }
 }
