9b7909f84ea369c255823163fb1f19f41659d142
Victor Matia <vmatir@gmail.com>
2024-03-09 14:53:15 +0000

    Fixed the error retrieval.
    Now error retrievals will never cause a double free.
    
    Signed-off-by: Victor Matia <vmatir@gmail.com>

--- Vmr.Sdl2.Net/Imports/Error.cs
+++ Vmr.Sdl2.Net/Imports/Error.cs
@@ -3,6 +3,8 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
+using Vmr.Sdl2.Net.Marshalling;
+
 namespace Vmr.Sdl2.Net.Imports;
 
 internal static partial class Sdl
@@ -10,7 +12,8 @@ internal static partial class Sdl
     [LibraryImport(
         LibraryName,
         EntryPoint = "SDL_GetError",
-        StringMarshalling = StringMarshalling.Utf8
+        StringMarshalling = StringMarshalling.Custom,
+        StringMarshallingCustomType = typeof(SdlOwnedUtf8StringMarshaller)
     )]
     [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
     public static partial string? GetError();
